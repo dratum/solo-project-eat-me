9f376c2de95433401f2bef13c8da89cad1b1656f
dratum <dratumzh187@gmail.com>
2024-03-23 13:34:42 +0300

    show favourites

--- components/pages/PrivatePage.jsx
+++ components/pages/PrivatePage.jsx
@@ -1,13 +1,13 @@
 const React = require('react');
 const Layout = require('../Layout');
 
-module.exports = function Private({ title, username, programs = [] }) {
+module.exports = function Private({ title, username, userPrograms = [] }) {
   return (
     <Layout title={title} username={username}>
       <div>
         Избранное:
-        {programs.map((program) => (
-          <div>
+        {userPrograms.map((program) => (
+          <div key={program.id}>
             <a href={`/program/${program.id}`}>{program.name}</a>
           </div>
         ))}
--- routers/views/private.router.js
+++ routers/views/private.router.js
@@ -1,15 +1,25 @@
 const router = require('express').Router();
 const PrivatePage = require('../../components/pages/PrivatePage');
+const { User_program } = require('../../db/models');
+const { Program } = require('../../db/models');
 
 router.get('/private', async (req, res) => {
-  const programs = [];
-  const { username } = res.locals;
+  const { username, id } = res.locals;
+
+  const programsArr = await User_program.findAll({
+    where: { user_id: id },
+    raw: true,
+  });
+  const userPrograms = await Program.findAll({
+    where: { id: programsArr.map((program) => program.program_id) },
+    raw: true,
+  });
 
   res.send(
     res.renderComponent(PrivatePage, {
       title: 'Личный кабинет',
       username,
-      programs,
+      userPrograms,
     })
   );
 });
